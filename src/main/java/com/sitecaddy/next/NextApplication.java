9f79d448f308dd79e4b0760c7d86d218cd1e1fe7
com-sitecaddy-flanagan-john <john@sitecaddy.com>
2019-03-22 11:56:49 -0400

    This is the first practical commit of the SiteCaddy NEXT project.

---
+++ src/main/java/com/sitecaddy/next/NextApplication.java
@@ -0,0 +1,13 @@
+package com.sitecaddy.next;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NextApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/sitecaddy/next/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.sitecaddy.next.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    @GetMapping("/world")
+    public String greeting() {
+        return "Hello World";
+    }
+}
